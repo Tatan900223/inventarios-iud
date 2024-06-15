b21d2ce79c91a3feb85033a00f63aec1dec9204f
jonathan.rios <jonathan.rios@est.iudigital.edu.co>
2024-06-15 13:22:14 -0500

    Inventarios-iud-structure

---
+++ db/db-connection-mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const getConnection = async () => {
+
+    try {
+
+        const url = 'mongodb://adminiud:<password>@ac-ejoqbfv-shard-00-00.qbplsal.mongodb.net:27017,ac-ejoqbfv-shard-00-01.qbplsal.mongodb.net:27017,ac-ejoqbfv-shard-00-02.qbplsal.mongodb.net:27017/?ssl=true&replicaSet=atlas-e3vo6q-shard-0&authSource=admin&retryWrites=true&w=majority&appName=AtlasCluster'
+
+        await mongoose.connect(url);
+
+        console.log('Conexion Exitosa');
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
