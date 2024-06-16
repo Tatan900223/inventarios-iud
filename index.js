fee2b3eae2df3105c48560a66426f345b861503f
jonathan.rios <jonathan.rios@est.iudigital.edu.co>
2024-06-16 11:21:39 -0500

    update

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(express.json())
 
 app.use('/usuario', require('./router/usuario'));
 app.use('/marca', require('./router/marca'));
+app.use('/tipoEquipo', require('./router/tipoEquipo'));
+app.use('/estadoEquipo', require('./router/estadoEquipo'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
