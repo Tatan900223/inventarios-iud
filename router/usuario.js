2873e5a44f59fa361be9dc135c588974a8d6b75a
jonathan.rios <jonathan.rios@est.iudigital.edu.co>
2024-06-15 21:53:26 -0500

    Update

--- router/usuario.js
+++ router/usuario.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const Usuario = require('../models/Usuario'); 
+const { validationResult, check } = require('express-validator');
+
+// Crear un nuevo usuario
+router.post('/', [
+
+    check('nombre', 'invalid.nombre').not().isEmpty(),
+    check('email', 'invalid.email').isEmail(),
+    check('estado', 'invalid.estado').isIn('Activo', 'Inactivo'),
+    check('password', 'invalid.password').not().isEmpty(),
+    check('rol', 'invalid.rol').isIn('Administrador', 'Docente'),
+
+],async (req, res) => {
+    
+    const existeUsuario = await Usuario.findOne({ email: req.body.email});
+        if (existeUsuario) {
+            return res.status(400).send('Email ya existe');
+        }
+});
+
 
 // Listar todos los usuarios
 router.get('/', async (req, res) => {
@@ -19,7 +38,7 @@ router.put('/:id', async (req, res) => {
         const usuarioActualizado = await Usuario.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.json(usuarioActualizado);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        console.log(error);
     }
 });
 
